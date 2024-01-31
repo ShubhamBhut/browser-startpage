403805022b1f50d227cd3040e530ee320f4e4ed9
ShubhamBhut <shubhamprecious1@gmail.com>
2024-01-31 08:07:00 +0530

    relative paths

--- src/js/theme.js
+++ src/js/theme.js
@@ -29,7 +29,7 @@ const colorSets = [
 function preloadImages() {
 	for (let i = 0; i < images.length; i++) {
 		const img = new Image();
-		img.src = "../src/images/" + images[i];
+		img.src = "src/images/" + images[i];
 	}
 }
 
@@ -41,7 +41,7 @@ function nextImage() {
 	updateColors(currentIndex);
 
 	setTimeout(() => {
-		imageElement.src = "../src/images/" + images[currentIndex];
+		imageElement.src = "src/images/" + images[currentIndex];
 		imageElement.style.opacity = 1;
 	}, 200); // Match the transition duration in style.css
 }
@@ -58,7 +58,7 @@ function updateColors() {
 updateColors(currentIndex);
 
 // Set the initial image
-document.getElementById("carouselImage").src = "../src/images/" + images[currentIndex];
+document.getElementById("carouselImage").src = "src/images/" + images[currentIndex];
 
 // Image is opacity 0 and text is translated off screen by default
 // Add the loaded class to the image and text to animate them in
